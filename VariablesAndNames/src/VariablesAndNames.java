e0618b7c75cb33f0246738ee5630a01f2ee57848
Kiara Panganiban <967433@learn.vsb.bc.ca>
2021-02-10 14:03:00 -0800

    VariablesAndNames

---
+++ VariablesAndNames/src/VariablesAndNames.java
@@ -0,0 +1,9 @@
+public class VariablesAndNames {
+    public static void main(String[] args) {
+        System.out.println(5>4);
+        System.out.println(4 < 5 || 8 < 7);
+
+        int x = 113;
+
+    }
+}
